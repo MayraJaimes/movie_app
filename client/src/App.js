e011782069593188aac26bd384655a56705273a2
faunarr <fhouchin@gmail.com>
2018-04-23 20:14:24 -0500

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
+
 
 class App extends Component {
   render() {
