e5662b2aafd46b81767f4c410a9b816a2dd806aa
faunarr <fhouchin@gmail.com>
2018-04-25 20:06:23 -0500

    adding stylings to options page

--- client/src/pages/Options/Options.js
+++ client/src/pages/Options/Options.js
@@ -10,16 +10,22 @@ class Options extends Component {
             <div>
 
                 < Navbar />
-
+            
                 <div className="container">
-                    <div className="row">
-                    <p className="col-md-12">What kind of movie do you want to watch?</p>
+                    <div className="row">                        
+                            <p className="headingQuestion">What kind of movie do you want to watch?</p>
+                            </div>
+                            <form className="form-inline">
+                                <button className="btn btn-primary pickMeUp" type="submit">Need a pick me up?</button>
+                                <button className="btn btn-primary misery" type="submit">Wallow in your misery.</button>
+                                <button className="btn btn-primary weDecide" type="submit">Let us decide for you!</button>
+                            </form>                                      
                     </div>
-                
+                 </div>
 
-                </div>
 
-            </div>
         )
     }
 }
+
+export default Options;
